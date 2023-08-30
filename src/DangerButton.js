d20f9cebdfdebcfe1f47b9d98aadf376c6879084
DashaSalauyova <113973560+DashaSalauyova@users.noreply.github.com>
2023-08-30 18:52:35 +0200

    Delete DangerButton.js

--- src/DangerButton.js
+++
@@ -1,8 +0,0 @@
-// import React, { Component } from 'react';
-// // Import a component from another file
-
-// const DangerButton = () => {
-//     return <button color="red" />;
-//   }
-
-// export default DangerButton;
